16438e33a3a477a8a79b4629ccc703d0b8774cd3
Khanh Nguyen <khanh.hm.nguyen03@gmail.com>
2025-12-28 13:15:04 +0700

    get event details by id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+import { IEvent } from "@/models";
+const BASE_URL = process.env.PUBLIC_BASE_URL;
 
-const page = () => {
+const page = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`);
+
+  const { events } = await response.json();
+
   return (
     <section>
       <h1 className="text-center">
@@ -10,6 +17,20 @@ const page = () => {
         Hackathons, Meetus, and Conferences, All in One Place
       </p>
       <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Feature Events</h3>
+
+        <ul className="events">
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title} className="list-none">
+                <EventCard {...event} />
+              </li>
+            ))}
+        </ul>
+      </div>
     </section>
   );
 };
